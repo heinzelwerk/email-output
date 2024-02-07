280a92b92839d456846192998ad68a4b521168ad
Jens Kleineheismann <heinzel@heinzelwerk.de>
2024-02-07 10:21:11 +0100

    UPD

--- scripts/email-output.py
+++ scripts/email-output.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
+import subprocess
 import sys
 
 PROG_NAME = 'email-output.py'
@@ -14,19 +15,24 @@ class Program:
         kwargs = {
             'prog': PROG_NAME,
             'description': 'Execute a command and send its output via email',
+            'usage': 'usage: %(prog)s [OPTIONS] [--] COMMAND [ARGS ...]',
+            'epilog': 'To prevent ARGS to be interpreted as OPTIONS to %(prog)s, add two dashes (--) before the COMMAND.'
         }
         parser = argparse.ArgumentParser(**kwargs)
         parser.add_argument('-V', '--version', action='version', version='%(prog)s {}'.format(VERSION))
-        parser.add_argument('-Q',
+        parser.add_argument('-C', '--no-combined',
                             action='store_true',
-                            help='do not send an email, if no output was produced.')
+                            help='do not combine stdout and stderr')
+        parser.add_argument('-E', '--no-empty',
+                            action='store_true',
+                            help='do not send an email, if no output was produced')
         parser.add_argument('-r', '--recipient', nargs=1, metavar='ADDR',
                             action='append',
-                            help='the email address, that the command output will be sent to.')
+                            help='the email address, that the command output will be sent to')
         parser.add_argument('command', nargs=1, metavar='COMMAND',
-                            help='the command, that will be executed.')
+                            help='the command, that will be executed')
         parser.add_argument('argv', nargs='*', metavar='ARGS',
-                            help='arguments to COMMAND.')
+                            help='arguments to COMMAND')
         return parser
 
     def _parse_args(self, argv=None):
@@ -40,9 +46,21 @@ class Program:
 
     def __call__(self, *args, **kwargs):
         argv = kwargs.get('argv', None)
-        cmd_args = self._parse_args(argv)
-        print(cmd_args)
-        return os.EX_OK
+        prog_args = self._parse_args(argv)
+        cmd = prog_args.command + prog_args.argv
+        if prog_args.no_combined:
+            stdout = subprocess.PIPE
+            stderr = subprocess.PIPE
+        else:
+            stdout = subprocess.PIPE
+            stderr = subprocess.STDOUT
+        ret = subprocess.run(cmd, stdout=stdout, stderr=stderr)
+        if ret.stdout:
+            sys.stdout.write(ret.stdout.decode(sys.stdout.encoding))
+        if ret.stderr:
+            sys.stderr.write(ret.stderr.decode(sys.stderr.encoding))
+        exitval = ret.returncode
+        return exitval
 
 
 def main(*args, **kwargs):
