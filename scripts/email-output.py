71d227a5159796687f6df69346e17ef44a061855
Jens Kleineheismann <heinzel@heinzelwerk.de>
2024-02-07 10:21:11 +0100

    Try to improve options

--- scripts/email-output.py
+++ scripts/email-output.py
@@ -11,7 +11,7 @@ import time
 from email.mime.text import MIMEText
 
 PROG_NAME = os.path.basename(__file__)
-VERSION = '1.0'
+VERSION = '1.1'
 
 
 class Program:
@@ -25,24 +25,52 @@ class Program:
     def _setup_argparser():
         kwargs = {
             'prog': PROG_NAME,
+            'formatter_class': argparse.RawDescriptionHelpFormatter,
             'description': 'Execute a command and send its output via email',
             'usage': 'usage: %(prog)s [OPTIONS] [--] COMMAND [ARGS ...]',
-            'epilog': 'To prevent ARGS to be interpreted as OPTIONS to %(prog)s,'
-                      ' add two dashes (--) before the COMMAND.'
+            'epilog': """To prevent ARGS to be interpreted as OPTIONS to %(prog)s,
+ add two dashes (--) before the COMMAND.
+
+Using the option --not-on-success will prevent sending an email if the command
+ returns zero (i.e. command succeed) no matter if output were produced or not.
+ Use this option if you want emails only if the command fails.
+
+Using the option --not-on-silence will prevent sending an email if the command
+ does not produce output no matter of the return code.
+ Use this option if you want emails only if the command produces output.
+ 
+If both options --not-on-silence and --not-on-success are given, an email will
+ only be sent if the command fails AND produce output.
+ This is likely not to be the behaviour you want.
+
+To get an email if the command fails OR produce output use the option
+ --not-on-silent-success
+"""
         }
         parser = argparse.ArgumentParser(**kwargs)
-        parser.add_argument('-V', '--version', action='version', version='%(prog)s {}'.format(VERSION))
-        parser.add_argument('-C', '--no-combined',
+        parser.add_argument('--version', '-V',
+                            action='version', version='%(prog)s {}'.format(VERSION))
+        parser.add_argument('--no-combined', '-C',
                             action='store_true',
                             help='do not combine stdout and stderr')
-        parser.add_argument('-E', '--no-empty',
+
+        parser.add_argument('--not-on-silence',
                             action='store_true',
-                            help='do not send an email, if command returns zero and no output was produced')
-        parser.add_argument('-r', '--recipient', nargs=1, metavar='ADDR',
+                            help='only send email if command produce output')
+        parser.add_argument('--not-on-success',
+                            action='store_true',
+                            help='only send email if command exit with non-zero'
+                                 ' return code (i.e. command fails)')
+        parser.add_argument('--not-on-silent-success',
+                            action='store_true',
+                            help='only send email if command exit with non-zero'
+                                 ' return code or produce any output')
+
+        parser.add_argument('--recipient', '-r', nargs=1, metavar='ADDR',
                             action='append',
                             help='the email address, that the command output will be sent to'
                                  ' (can be used multiple times)')
-        parser.add_argument('-s', '--subject', nargs=1, metavar='TEXT',
+        parser.add_argument('--subject', '-s', nargs=1, metavar='TEXT',
                             help='the email subject')
         parser.add_argument('command', nargs=1, metavar='COMMAND',
                             help='the command, that will be executed')
@@ -62,6 +90,16 @@ class Program:
         tz_name = localtime.tm_zone
         tz_offset = localtime.tm_gmtoff
         return datetime.timezone(datetime.timedelta(seconds=tz_offset), name=tz_name)
+
+    @staticmethod
+    def _write_to_stdout(fp):  # pylint: disable=invalid-name
+        fp.seek(0)
+        sys.stdout.write(fp.read().decode(sys.stdout.encoding))
+
+    @staticmethod
+    def _write_to_stderr(fp):  # pylint: disable=invalid-name
+        fp.seek(0)
+        sys.stderr.write(fp.read().decode(sys.stderr.encoding))
 
     @staticmethod
     def _send_email(body, subject=None, to_hdr=None, from_hdr=None):
@@ -91,18 +129,20 @@ class Program:
 
         command = prog_args.command + prog_args.argv
         combine_output = not prog_args.no_combined
-        send_mail = not prog_args.no_empty
         if prog_args.subject:
             mail_subject = prog_args.subject[0]
         else:
             mail_subject = ' '.join(command)
 
+        # Create one or two temporary files to buffer output from the command
+        # (depending if we should combine stdout and stderr or not)
         stdout_buf = tempfile.TemporaryFile()
         if combine_output:
             stderr_buf = subprocess.STDOUT
         else:
             stderr_buf = tempfile.TemporaryFile()
 
+        # Run the command and store time before and after
         timezone = self._get_local_timezone()
         command_started_at = datetime.datetime.now(timezone)
         try:
@@ -119,22 +159,29 @@ class Program:
             command_returncode = e.returncode
         command_finished_at = datetime.datetime.now(timezone)
 
-        if command_returncode:
-            send_mail = True
-
+        # Write the command output to our output
+        has_output = False
         if stdout_buf.tell():
-            send_mail = True
-            stdout_buf.seek(0)
-            sys.stdout.write(stdout_buf.read().decode(sys.stdout.encoding))
+            has_output = True
+            self._write_to_stdout(stdout_buf)
 
         if not combine_output and stderr_buf.tell():
-            send_mail = True
-            stderr_buf.seek(0)
-            sys.stderr.write(stderr_buf.read().decode(sys.stderr.encoding))
+            has_output = True
+            self._write_to_stderr(stderr_buf)
 
-        if send_mail:
+        # Send an email if command returned non-zero (or user want it anyways)
+        # DNF
+        #if (has_output and command_returncode) or \
+        #        (has_output and not prog_args.not_on_success) or \
+        #        (command_returncode and prog_args.not_on_silence) or \
+        #        (prog_args.not_on_success and prog_args.not_on_silence and prog_args.not_on_silent_success):
+        # KNF
+        if (has_output or not prog_args.not_on_silence) and \
+                (command_returncode or not prog_args.not_on_success) and \
+                (has_output or command_returncode or not prog_args.not_on_silent_success):
             mail_body = ''
 
+            # Add some information about the command to the mail
             timestamp_format = '%a, %d.%m.%Y %H:%M:%S.%f %Z (%z)'
             mail_intro = """Host:        {hostname}
 Command:     {command}
@@ -152,6 +199,7 @@ Return code: {returncode}
 
             mail_body += mail_intro
 
+            # Add the command output to the mail
             if combine_output:
                 mail_body += 'Output:\n'
             else:
@@ -167,6 +215,7 @@ Return code: {returncode}
                 for line in stderr_buf:
                     mail_body += line.decode('utf-8')
 
+            # Who should receive a mail
             if prog_args.recipient:
                 # Flatten two dimensional list from argparse
                 recipients = []
@@ -176,9 +225,11 @@ Return code: {returncode}
             else:
                 recipients = [None]
 
+            # Send mails to all recipients
             for recipient in recipients:
                 self._send_email(mail_body, subject=mail_subject, to_hdr=recipient)
 
+        # Close our buffers -> the temporary files will be deleted automatically
         stdout_buf.close()
         if stderr_buf != subprocess.STDOUT:
             stderr_buf.close()
